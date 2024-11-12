990d80f92e8afc4089f725dc9ae7571071e6500d
wert1ko <novikknow77@gmail.com>
2024-11-12 15:44:27 +0200

    add salt

--- config/admin.ts
+++ config/admin.ts
@@ -1,9 +1,9 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'yourAdminJWTSecret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'defaultSalt'),
   },
   transfer: {
     token: {
